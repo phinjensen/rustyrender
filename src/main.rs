d15f9da88610efaab312b0165513ea3f8cc428c2
Phineas Jensen <phin@endpointdev.com>
2022-05-22 11:43:25 -0600

    Remove unused dependency and use vflip

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ const RED: RGB = RGB { r: 255, g: 0, b: 0 };
 fn main() {
     let mut image: Image<RGB> = Image::new(100, 100);
     image.set(52, 41, RED).unwrap();
-    image.write_to_file("output.tga", false, true).unwrap();
+    image.write_to_file("output.tga", true, true).unwrap();
 }
