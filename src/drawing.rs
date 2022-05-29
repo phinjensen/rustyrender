5a4c1a3b178ac6d7744660a1ae5de52bd19ad653
Phineas Jensen <phin@endpointdev.com>
2022-05-29 11:18:16 -0600

    Initial attempt at filling triangles
    Kinda works, but draws outside of the line drawing algorithm's areas

--- src/drawing.rs
+++ src/drawing.rs
@@ -1,7 +1,14 @@
-use crate::geometry::Vec2;
-use crate::tga::{ColorSpace, Image};
+use core::f32;
 
-pub fn line<T: ColorSpace + Copy>(a: Vec2<isize>, b: Vec2<isize>, image: &mut Image<T>, color: T) {
+use crate::geometry::Vec2;
+use crate::tga::{ColorSpace, Image, RGB};
+
+pub fn line<T: ColorSpace + Copy>(
+    a: &Vec2<isize>,
+    b: &Vec2<isize>,
+    image: &mut Image<T>,
+    color: T,
+) {
     let mut x0 = a.x;
     let mut y0 = a.y;
     let mut x1 = b.x;
@@ -43,4 +50,67 @@ pub fn line<T: ColorSpace + Copy>(a: Vec2<isize>, b: Vec2<isize>, image: &mut Im
         }
         x += 1;
     }
+}
+
+const RED: RGB = RGB { r: 255, g: 0, b: 0 };
+const GREEN: RGB = RGB { r: 0, g: 255, b: 0 };
+
+pub fn triangle(
+    //<T: ColorSpace + Copy>(
+    t0: &Vec2<isize>,
+    t1: &Vec2<isize>,
+    t2: &Vec2<isize>,
+    image: &mut Image<RGB>,
+    color: RGB,
+) {
+    let mut t0 = t0;
+    let mut t1 = t1;
+    let mut t2 = t2;
+    if t0.y > t1.y {
+        (t0, t1) = (t1, t0);
+    }
+    if t0.y > t2.y {
+        (t0, t2) = (t2, t0);
+    }
+    if t1.y > t2.y {
+        (t1, t2) = (t2, t1);
+    }
+    println!("Vertices: {:?}, {:?}, {:?}", t0, t1, t2);
+    let arun = (t0.y - t2.y) as f32 / (t0.x - t2.x) as f32;
+    let brun = (t0.y - t1.y) as f32 / (t0.x - t1.x) as f32;
+    let aintercept = t0.y as f32 - arun * t0.x as f32;
+    let bintercept = t0.y as f32 - brun * t0.x as f32;
+    for y in t0.y..t1.y {
+        line(
+            &Vec2 {
+                x: ((y as f32 - aintercept) / arun) as isize,
+                y,
+            },
+            &Vec2 {
+                x: ((y as f32 - bintercept) / brun) as isize,
+                y,
+            },
+            image,
+            RED,
+        );
+    }
+    let brun = (t1.y - t2.y) as f32 / (t1.x - t2.x) as f32;
+    let bintercept = t1.y as f32 - brun * t1.x as f32;
+    for y in t1.y..t2.y {
+        line(
+            &Vec2 {
+                x: ((y as f32 - aintercept) / arun) as isize,
+                y,
+            },
+            &Vec2 {
+                x: ((y as f32 - bintercept) / brun) as isize,
+                y,
+            },
+            image,
+            RED,
+        );
+    }
+    line(t0, t1, image, RED);
+    line(t1, t2, image, RED);
+    line(t2, t0, image, GREEN);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::isize;
 
-use rustyrender::drawing::line;
+use rustyrender::drawing::{line, triangle};
 use rustyrender::geometry::Vec2;
 use rustyrender::model::Model;
 use rustyrender::tga::{Image, RGB};
@@ -12,27 +12,21 @@ const WHITE: RGB = RGB {
 };
 
 fn main() {
-    let width = 800;
-    let height = 800;
+    let width = 200;
+    let height = 200;
     let mut image: Image<RGB> = Image::new(width, height);
-    let model = Model::from("obj/african_head.obj").unwrap();
 
-    for i in 0..model.num_faces() {
-        let face = model.face(i);
-        for j in 0..3 {
-            let v0 = model.vertex(face[j]);
-            let v1 = model.vertex(face[(j + 1) % 3]);
-            let a = Vec2 {
-                x: ((v0.x + 1.) * (width as f32) / 2.) as isize,
-                y: ((v0.y + 1.) * (height as f32) / 2.) as isize,
-            };
-            let b = Vec2 {
-                x: ((v1.x + 1.) * (width as f32) / 2.) as isize,
-                y: ((v1.y + 1.) * (height as f32) / 2.) as isize,
-            };
-            line(a, b, &mut image, WHITE)
-        }
-    }
+    let t0: Vec<Vec2<isize>> = vec![Vec2::from(10, 70), Vec2::from(50, 160), Vec2::from(70, 80)];
+    let t1: Vec<Vec2<isize>> = vec![Vec2::from(180, 50), Vec2::from(150, 1), Vec2::from(70, 180)];
+    let t2: Vec<Vec2<isize>> = vec![
+        Vec2::from(180, 150),
+        Vec2::from(120, 160),
+        Vec2::from(130, 180),
+    ];
+
+    triangle(&t0[0], &t0[1], &t0[2], &mut image, WHITE);
+    triangle(&t1[0], &t1[1], &t1[2], &mut image, WHITE);
+    triangle(&t2[0], &t2[1], &t2[2], &mut image, WHITE);
 
     image.write_to_file("output.tga", true, true).unwrap();
 }
